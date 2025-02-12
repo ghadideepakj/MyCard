d1806e8c28647b7e0dc5df331b3a18bd8b563096
“Deepak” <deepak.ghadi@idsnext.com>
2025-02-12 16:28:34 +0530

    Initial Commit

---
+++ Card SwiftUI/Card_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_SwiftUIApp.swift
+//  Card SwiftUI
+//
+//  Created by Deepak Ghadi on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Card_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Card SwiftUI
+//
+//  Created by Deepak Ghadi on 12/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
